6ba36ae2a24cda24db3a3fabe0490f27150a3cb2
Zetchu <shmungula@gmail.com>
2026-01-30 09:57:19 +0100

    .

--- resources/views/admin/series/index.blade.php
+++ resources/views/admin/series/index.blade.php
@@ -6,10 +6,10 @@
             Export Data
         </a>
 
-        <a href="{{ route('admin.series.create') }}"
-           class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
-            Create New
-        </a>
+        {{--        <a href="{{ route('admin.series.create') }}"--}}
+        {{--           class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">--}}
+        {{--            Create New--}}
+        {{--        </a>--}}
     </div>
 
     <div class="bg-[#1a1a1a] overflow-hidden shadow-xl rounded-2xl border border-white/5">
